f5889843a45fcab72542775f63fbef8ea0568551
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2025-02-26 16:46:42 -0300

    refactor tailwind configuration for improved styling and consistency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,38 +8,38 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        main: "var(--main)",
-        overlay: "var(--overlay)",
-        bg: "var(--bg)",
-        bw: "var(--bw)",
-        blank: "var(--blank)",
-        text: "var(--text)",
-        mtext: "var(--mtext)",
-        border: "var(--border)",
-        ring: "var(--ring)",
-        ringOffset: "var(--ring-offset)",
-
-        secondaryBlack: "#212121",
-      },
-      borderRadius: {
-        base: "0px",
-      },
-      boxShadow: {
-        shadow: "var(--shadow)",
-      },
-      translate: {
-        boxShadowX: "-6px",
-        boxShadowY: "6px",
-        reverseBoxShadowX: "6px",
-        reverseBoxShadowY: "-6px",
-      },
-      fontWeight: {
-        base: "500",
-        heading: "700",
-      },
+  extend: {
+    colors: {
+      main: 'var(--main)',
+      overlay: 'var(--overlay)',
+      bg: 'var(--bg)',
+      bw: 'var(--bw)',
+      blank: 'var(--blank)',
+      text: 'var(--text)',
+      mtext: 'var(--mtext)',
+      border: 'var(--border)',
+      ring: 'var(--ring)',
+      ringOffset: 'var(--ring-offset)',
+      
+      secondaryBlack: '#212121', 
+    },
+    borderRadius: {
+      base: '4px'
+    },
+    boxShadow: {
+      shadow: 'var(--shadow)'
+    },
+    translate: {
+      boxShadowX: '-4px',
+      boxShadowY: '4px',
+      reverseBoxShadowX: '4px',
+      reverseBoxShadowY: '-4px',
+    },
+    fontWeight: {
+      base: '500',
+      heading: '900',
     },
   },
+},
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
