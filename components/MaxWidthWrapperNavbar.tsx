adf6f166142b34df10933743ba89264178a31022
Rodrigo Huajamaita <rhuajamaita@gmail.com>
2025-07-28 08:45:41 -0300

    fix: change wrapper from div to header for semantic structure

--- components/MaxWidthWrapperNavbar.tsx
+++ components/MaxWidthWrapperNavbar.tsx
@@ -9,14 +9,14 @@ const MaxWidthWrapperNavbar = ({
   children: ReactNode;
 }) => {
   return (
-    <div
+    <header
       className={cn(
         "h-full mx-auto w-full max-w-screen-2xl px-2.5 md:px-20",
         className
       )}
     >
       {children}
-    </div>
+    </header>
   );
 };
 
